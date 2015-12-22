324ea1ff8c922948ecedc99b0305d1821670a723
JobM <jobmwaniki18@gmail.com>
2015-12-22 15:06:47 +0300

    added migrations folder to gitignore

--- Wiki/models.py
+++ Wiki/models.py
@@ -7,7 +7,7 @@ class Shared(models.Model):
     title = models.CharField(blank=False, max_length=100)
     post = models.TextField(blank=False)
     views = models.IntegerField(blank=True, default=0)
-    category = models.CharField(blank=True, default='others')
+    category = models.CharField(blank=True, max_length=100, default='others')
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
     class Meta:
