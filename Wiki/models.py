52561d004c69aecd680a4151640a56e43fc02d49
JobM <jobmwaniki18@gmail.com>
2015-12-22 14:57:31 +0300

    created wiki article model

--- Wiki/models.py
+++ Wiki/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Shared(models.Model):
+    title = models.CharField(blank=False, max_length=100)
+    post = models.TextField(blank=False)
+    views = models.IntegerField(blank=True, default=0)
+    category = models.CharField(blank=True, default='others')
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract=True
+
+
+class Article(Shared):
+    slug = models.SlugField(unique=True)
+    created = models.BooleanField(default=False)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'wiki: article', (self.slug,)
+
+
+class Draft(Shared):
+    created = models.BooleanField(default=False)
